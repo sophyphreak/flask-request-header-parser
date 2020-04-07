0219b45db9c7084049f03ea0ab82ab51d91281e3
Andrew Horn <arhorn@smcm.edu>
2020-04-06 21:44:21 -0500

    functions locally

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class RequestHeaderParser(Resource):
+    def get(self):
+        return { 
+            'ipaddress': request.remote_addr,
+            'language': str(request.accept_languages),
+            'software': str(request.user_agent)
+        }
+
+
+api.add_resource(RequestHeaderParser, '/api/whoami/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
