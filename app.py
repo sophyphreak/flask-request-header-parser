0d585f6a233f199ba37e6b0bef3573c8d51e088f
Andrew Horn <arhorn@smcm.edu>
2020-04-07 23:39:31 -0500

    ran black

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
 class RequestHeaderParser(Resource):
     def get(self):
-        return { 
-            'ipaddress': request.remote_addr,
-            'language': str(request.accept_languages),
-            'software': str(request.user_agent)
+        return {
+            "ipaddress": request.remote_addr,
+            "language": str(request.accept_languages),
+            "software": str(request.user_agent),
         }
 
 
-api.add_resource(RequestHeaderParser, '/api/whoami/')
+api.add_resource(RequestHeaderParser, "/api/whoami/")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
